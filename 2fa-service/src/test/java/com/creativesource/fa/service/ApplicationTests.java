c3f36412e32094fd48016f80c7e1843bacb47b47
Adriano Santos <adriano.santos@notifye.io>
2018-10-08 23:24:21 -0300

    Fix. Add support to configuration class in tests files

--- 2fa-service/src/test/java/com/creativesource/fa/service/ApplicationTests.java
+++ 2fa-service/src/test/java/com/creativesource/fa/service/ApplicationTests.java
@@ -5,8 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.creativesource.twofactor.service.Application;
+import com.creativesource.twofactor.service.ApplicationSecurityConfig;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {Application.class, ApplicationSecurityConfig.class})
 public class ApplicationTests {
 
 	@Test
