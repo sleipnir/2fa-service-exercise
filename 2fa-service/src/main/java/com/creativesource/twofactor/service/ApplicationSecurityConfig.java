df239d957ef1ceb6b7c0ee4069a4f0587da3da46
Adriano Santos <adriano.santos@notifye.io>
2018-10-08 22:24:35 -0300

    Feat. Add hook controller logic and security exception

--- 2fa-service/src/main/java/com/creativesource/twofactor/service/ApplicationSecurityConfig.java
+++ 2fa-service/src/main/java/com/creativesource/twofactor/service/ApplicationSecurityConfig.java
@@ -2,6 +2,7 @@ package com.creativesource.twofactor.service;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
@@ -27,6 +28,7 @@ public class ApplicationSecurityConfig {
 	@Bean
 	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 		http.csrf().disable().authorizeExchange()
+			.pathMatchers(HttpMethod.POST, "/auth/tokens/events").permitAll()
 			.anyExchange()
 				.authenticated()
 					.and().httpBasic();
---
+++ 2fa-service/src/main/java/com/creativesource/twofactor/service/web/WebHookController.java
@@ -0,0 +1,23 @@
+package com.creativesource.twofactor.service.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RestController
+@RequestMapping
+public class WebHookController {
+	
+	@PostMapping("/auth/tokens/events")
+	public Mono<ResponseEntity<?>> event(@RequestBody String request){
+		log.info("Request {}", request);
+		return Mono.just(ResponseEntity.ok().build());
+	}
+	
+}
